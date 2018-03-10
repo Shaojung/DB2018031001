52251528925396e3eaa92ef2c1b1f1ba9e9d9dcb
Shaojung <shaojung@gmail.com>
2018-03-10 11:26:56 +0800

    put buttons

--- app/src/main/java/com/example/student/db2018031001/MainActivity.java
+++ app/src/main/java/com/example/student/db2018031001/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.student.db2018031001;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     File p;
@@ -16,5 +23,30 @@ public class MainActivity extends AppCompatActivity {
         p = getFilesDir();
         myFile = new File(p, "myfile.txt");
         Log.d("MyFile", myFile.toString());
+    }
+    public void clickRead(View v)
+    {
+        try {
+            FileReader fr = new FileReader(myFile);
+            BufferedReader br = new BufferedReader(fr);
+            String str = br.readLine();
+            Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void clickWrite(View v)
+    {
+        try {
+            FileWriter fw = new FileWriter(myFile);
+            fw.write("Hello World");
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
